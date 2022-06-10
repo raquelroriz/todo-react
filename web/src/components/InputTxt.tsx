49b264f6ce134f2da5288f1b0f8b14102a6cd2e7
Maria Raquel <maria.raquel.roriz@gmail.com>
2022-06-10 18:46:24 -0300

    #5: capturar e resetar campo

--- web/src/components/InputTxt.tsx
+++ web/src/components/InputTxt.tsx
@@ -1,19 +1,31 @@
 import React, { useState } from 'react';
 
 export function InputTxt() {
-  const [state, setState] = useState('');
+  //const [pressedKey, setPressedKey] = useState('');
+  const [newTodo, setNewTodo] = useState('');
+
   const keyHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    setState(event.key);
+    //setPressedKey(event.key);
+    if (event.key == 'Enter') {
+      setNewTodo('');
+    }
   };
+
+  const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTodo(event.target.value);
+  };
+
   return (
     <div>
-      a tecla pressionada foi:{state}
+      <p>O texto foi:{newTodo}</p>
       <input
         onKeyDown={keyHandler}
+        onChange={onChangeHandler}
         type="text"
         name="newtodo"
         placeholder="new ToDos"
         className="input input-bordered input-primary w-full"
+        value={newTodo}
       />
     </div>
   );
