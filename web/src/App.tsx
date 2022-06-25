762d1e6f84f16a2ec0d3d186344a17b0a95e98c2
Maria Raquel <maria.raquel.roriz@gmail.com>
2022-06-25 13:33:38 -0300

    #11: Logica de filtro

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import { InputTxt } from './components/InputTxt';
 import { Listagem } from './components/Listagem';
 import { Contador } from './components/Contador';
+import { Filtro } from './components/Filtro';
 import React, { useState } from 'react';
 
 interface Todo {
@@ -11,7 +12,7 @@ interface Todo {
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
-
+  const [filter, setFilter] = useState('');
   const handlerSave = (newTitle: string, newCompleted: boolean) => {
     let newTodo: Todo = {
       title: newTitle,
@@ -21,6 +22,10 @@ function App() {
     setTodos([...todos, newTodo]);
   };
 
+  const handlerFilter = (newFilter: string) => {
+    setFilter(newFilter);
+  };
+
   return (
     <>
       <div className="flex justify-center text-5xl font-mono font-bold">
@@ -28,30 +33,13 @@ function App() {
       </div>
       <div className="flex justify-center ">
         <div className="grid justify-items-center grid-cols-1 border-2 border-indigo-500/50 border-gray-300 w-8/12 shadow-2xl py-8">
-          {/* 
-          linha responsavel pelo preenchimento new todo
-          */}
-
           <div className="linha1 w-11/12 ">
             <InputTxt onPressEnter={handlerSave}></InputTxt>
           </div>
 
-          {/* 
-          linha responsavel pela marcação do que está feito ou a fazer
-          novo fica com a checkbox livre (active todo)
-          feito checkbox preenchido e riscado (done todo)
-          */}
-
           <div className="linha2 w-11/12 py-8">
             <Listagem todoList={todos}></Listagem>
           </div>
-
-          {/* 
-          linha responsavel pelo rodape
-          primeiro mostra quantos itens ainda faltam (contador)
-          segundo combo de 3 botões, 1° mostra todos(All), 2° apenas os ativos(Active) e 3° os completos (Completed)
-          terceiro botão para limpar tudo (Clear completed)
-          */}
 
           <div className="linha3 w-11/12">
             <div className="flex flex-row items-center self-center">
@@ -60,19 +48,7 @@ function App() {
               </div>
 
               <div className="basis-8/12 container mx-auto">
-                <div className="d-inline-flex p-3 justify-center flex flex-row gap-1 items-center ">
-                  <div className="basis-2/12">
-                    <button className="btn btn-sm">All</button>
-                  </div>
-
-                  <div className="basis-2/12">
-                    <button className="btn btn-sm">Active</button>
-                  </div>
-
-                  <div className="basis-2/12">
-                    <button className="btn btn-sm">Completed</button>
-                  </div>
-                </div>
+                <Filtro onPressFilter={handlerFilter}></Filtro>
               </div>
 
               <div className="basis-2/12 gap-5">
